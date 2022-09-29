ed4200854d4a66a7a70f9a053e1f744bba947ca1
Zakiah07 <zakiah_bloom@yahoo.com>
2022-09-29 08:43:33 +0800

    hooks included

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,51 @@
 import './App.css';
-import { Greet } from './components/Greet';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
-import { Status } from './components/Status';
+import { Container } from './components/Container';
+// import { Input } from './components/Input';
+// import { Greet } from './components/Greet';
+// import { Heading } from './components/Heading';
+// import { Oscar } from './components/Oscar';
+// import { Person } from './components/Person';
+// import { PersonList } from './components/PersonList';
+// import { Status } from './components/Status';
+// import { Button } from './components/Button';
 
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: "Wayne"
-  }
+  // const personName = {
+  //   first: "Bruce",
+  //   last: "Wayne"
+  // }
 
-  const nameList = [
-    {
-      first: "Bruce",
-      last: "Wayne"
-    },
-    {
-      first: "Clark",
-      last: "Kent"
-    },
-    {
-      first: "Princess",
-      last: "Diana"
-    }
-  ]
+  // const nameList = [
+  //   {
+  //     first: "Bruce",
+  //     last: "Wayne"
+  //   },
+  //   {
+  //     first: "Clark",
+  //     last: "Kent"
+  //   },
+  //   {
+  //     first: "Princess",
+  //     last: "Diana"
+  //   }
+  // ]
 
   return (
     <div className="App">
-      <Greet name="Zakiah" messageCount={20} isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
-      <Status status='loading' />
+      {/* <Greet name="Zakiah" messageCount={20} isLoggedIn={false} /> */}
+      {/* <Person name={personName} /> */}
+      {/* <PersonList names={nameList} /> */}
+      {/* <Status status='success' />
+      <Heading>Placeholder Text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
+      </Oscar>
+      <Greet name="Zakiah" isLoggedIn={false} /> */}
+      {/* <Button handleClick={(event, id) => {
+        console.log("Button clicked", event, id)
+      }} />
+      <Input value="" handleChange={(event) => console.log(event)} /> */}
+      <Container styles={{ border: '1px solid black', padding: '1rem' }} />
     </div>
   );
 }
